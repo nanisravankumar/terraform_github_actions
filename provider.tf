5f7c931c9e1808174d9157ba35e3a74bab14a15b
nanisravankumar <pulle.sravankumar@gmail.com>
2025-05-06 11:04:37 +0800

    Add subscription_id to Terraform backend configuration

--- provider.tf
+++ provider.tf
@@ -11,6 +11,7 @@ terraform {
     container_name       = "terragithubactions"
     key                  = "terraform.tfstate"
     resource_group_name  = "testrg"
+    subscription_id      = "8275bd32-2df4-4467-9201-597dbf8e04a5"
   }
 }
 
