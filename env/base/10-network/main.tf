0d98c2ab72f46d522e7b8108f7ddf238b1fd1a5f
nanisravankumar <pulle.sravankumar@gmail.com>
2025-05-08 11:49:40 +0800

    Uncomment backend configuration in Terraform main.tf and simplify GitHub Actions workflow steps

--- env/base/10-network/main.tf
+++ env/base/10-network/main.tf
@@ -1,10 +1,10 @@
 terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rg-econ-prod-state"
-#     storage_account_name = "srsteconprodstate"
-#     container_name       = "stceconprodstate"
-#     key                  = "10-network.tfstate"
-#   }
+  backend "azurerm" {
+    resource_group_name  = "rg-econ-prod-state"
+    storage_account_name = "srsteconprodstate"
+    container_name       = "stceconprodstate"
+    key                  = "10-network.tfstate"
+  }
 }
 provider "azurerm" {
   features {}
