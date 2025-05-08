78dbfbf8e94461265c65714c3dfd57be27778301
nanisravankumar <pulle.sravankumar@gmail.com>
2025-05-08 14:54:55 +0800

    Refactor GitHub Actions workflow to remove environment variables and streamline Terraform commands

--- env/base/10-network/main.tf
+++ env/base/10-network/main.tf
@@ -4,11 +4,13 @@ terraform {
     storage_account_name = "srsteconprodstate"
     container_name       = "stceconprodstate"
     key                  = "10-network.tfstate"
+    subscription_id      = "8275bd32-2df4-4467-9201-597dbf8e04a5"
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id      = "8275bd32-2df4-4467-9201-597dbf8e04a5"
 }
 
 resource "azurerm_resource_group" "econcloud" {
