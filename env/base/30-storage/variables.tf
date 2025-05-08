fa7448b1420f5895a331d1154bb87a1b49cc3936
nanisravankumar <pulle.sravankumar@gmail.com>
2025-05-08 15:51:37 +0800

    Add variables for resource configuration and update existing variable definitions

--- env/base/30-storage/variables.tf
+++ env/base/30-storage/variables.tf
@@ -1,3 +1,105 @@
+# # ------------------
+# # General
+# # ------------------
+# variable "location" {
+#   type        = string
+#   description = "Location/Region that will be used for resources"
+# }
+
+# variable "prefix" {
+#   type        = string
+#   description = "The prefix for the resources created in the specified Azure Resource Group"
+# }
+
+# variable "environment" {
+#   type        = string
+#   description = "The environment name that will be used for tagging"
+# }
+
+# variable "tags" {
+#   type        = map(string)
+#   default     = {}
+#   description = "Any tags that should be present on the resources"
+# }
+
+# variable "common_tags" {
+#   description = "Common tags for all resources"
+#   type        = map(string)
+#   default = {
+#     "environment"              = "PROD"
+#     "managedBy"                = "Terraform"
+#     "mbmTechnicalOwnerContact" = "ajai.sreenilayam@mercedes-benz.com"
+#   }
+# }
+
+# # eCon Apps Vars
+
+# variable "esign" {
+#   type = object({
+#     isenabled = bool
+#     configs = list(string)
+#   })
+#   default = {
+#     isenabled   = true
+#     string_list = [""]
+#   }
+# }
+
+# variable "archive" {
+#   type = object({
+#     isenabled = bool
+#     configs = list(string)
+#   })
+#   default = {
+#     isenabled   = true
+#     string_list = [""]
+#   }
+# }
+
+# variable "camunda" {
+#   type = object({
+#     isenabled = bool
+#     configs = list(string)
+#   })
+#   default = {
+#     isenabled   = true
+#     string_list = [""]
+#   }
+# }
+
+# variable "appworks" {
+#   type = object({
+#     isenabled = bool
+#     configs = list(string)
+#   })
+#   default = {
+#     isenabled   = true
+#     string_list = [""]
+#   }
+# }
+
+# variable "docgen" {
+#   type = object({
+#     isenabled = bool
+#     configs = list(string)
+#   })
+#   default = {
+#     isenabled   = true
+#     string_list = [""]
+#   }
+# }
+
+# variable "ocr" {
+#   type = object({
+#     isenabled = bool
+#     configs = list(string)
+#   })
+#   default = {
+#     isenabled   = true
+#     string_list = [""]
+#   }
+# }
+
 # ------------------
 # General
 # ------------------
@@ -41,7 +143,7 @@ variable "esign" {
   })
   default = {
     isenabled   = true
-    string_list = [""]
+    configs     = [""]
   }
 }
 
@@ -52,7 +154,7 @@ variable "archive" {
   })
   default = {
     isenabled   = true
-    string_list = [""]
+    configs     = [""]
   }
 }
 
@@ -63,7 +165,7 @@ variable "camunda" {
   })
   default = {
     isenabled   = true
-    string_list = [""]
+    configs     = [""]
   }
 }
 
@@ -74,7 +176,7 @@ variable "appworks" {
   })
   default = {
     isenabled   = true
-    string_list = [""]
+    configs     = [""]
   }
 }
 
@@ -85,7 +187,7 @@ variable "docgen" {
   })
   default = {
     isenabled   = true
-    string_list = [""]
+    configs     = [""]
   }
 }
 
@@ -96,6 +198,6 @@ variable "ocr" {
   })
   default = {
     isenabled   = true
-    string_list = [""]
+    configs     = [""]
   }
 }
